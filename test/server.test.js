b521919298399d2b047929e833bdb5c700fd5bf8
Larry Cessna <larry.cessna@outlook.com>
2017-10-28 10:08:18 -0700

    Routes.js GET complete. Testing for GET also done.

--- test/server.test.js
+++ test/server.test.js
@@ -1,4 +1,39 @@
 // Test for these things:
+'use strict';
+
+const Note = require('../lib/note.js');
+const app = require('../lib/routes.js');
+const request = require('superagent');
+const expect = require ('expect');
+const server = app.listen(3000);
+
+describe('Testing for GET', () => {
+
+  after((done) => {
+    server.close();
+    done();
+  });
+
+  it('should send an error if note is not found', (done) => {
+    request.get('localhost:3000/api/notes/123').end(function(err, res) {
+      expect(res.text).toEqual('Note not found');
+      done();
+    });
+  });
+  it('should send the note when correct id is presented', (done) => {
+    request.get('localhost:3000/api/notes/David').end(function(err, res) {
+      expect(res.text).toEqual('sucks');
+      done();
+    });
+  });
+  it('should send an error if you do not send an id', (done) => {
+    request.get('localhost:3000/api/notes/').end(function(err, res) {
+      expect(res.text).toEqual('You did not send an id');
+      done();
+    });
+  });
+
+});
 
 // GET /api/notes?uuid= get note by uuid
         // 404 if note is not found
